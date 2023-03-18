e37d00ebaac80a2241e98b21da9ee24045b76e2d
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-18 14:39:57 +0900

    CardStackView UI 구현 및 Adapter 연결

--- app/src/main/java/com/nunu/android_dating_app/MainActivity.kt
+++ app/src/main/java/com/nunu/android_dating_app/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.nunu.android_dating_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.nunu.android_dating_app.slider.CardStackAdapter
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.CardStackView
+import com.yuyakaido.android.cardstackview.Direction
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cardStackAdpater : CardStackAdapter
+    lateinit var manager : CardStackLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cardStackView = findViewById<CardStackView>(R.id.cardStackView)
+
+        manager = CardStackLayoutManager(baseContext, object : CardStackListener{
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+
+            }
+
+            override fun onCardRewound() {
+
+            }
+
+            override fun onCardCanceled() {
+
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+
+            }
+
+        })
+
+        val testList = mutableListOf<String>()
+        testList.add("a")
+        testList.add("b")
+        testList.add("c")
+
+        cardStackAdpater = CardStackAdapter(baseContext,testList)
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = cardStackAdpater
+
     }
+
 }
