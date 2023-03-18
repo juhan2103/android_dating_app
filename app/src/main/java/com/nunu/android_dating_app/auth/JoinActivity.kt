15a882eb35f4bafed0e578695b66bd88e3311ed9
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-18 23:19:41 +0900

    회원가입 UI 및 기능 구현

--- app/src/main/java/com/nunu/android_dating_app/auth/JoinActivity.kt
+++ app/src/main/java/com/nunu/android_dating_app/auth/JoinActivity.kt
@@ -3,15 +3,49 @@ package com.nunu.android_dating_app.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.nunu.android_dating_app.MainActivity
 import com.nunu.android_dating_app.R
 
 class JoinActivity : AppCompatActivity() {
+
+    private var TAG = "JoinActivity"
+
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
-        
+
+        // Initialize Firebase Auth
+
+        auth = Firebase.auth
+
+        val joinBtn = findViewById<Button>(R.id.joinBtn)
+        joinBtn.setOnClickListener {
+
+            val email = findViewById<TextInputEditText>(R.id.emailArea)
+            val pwd = findViewById<TextInputEditText>(R.id.pwdArea)
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "createUser")
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+
+                    } else {
+
+                    }
+                }
+        }
+
     }
 }
