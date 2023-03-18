26133f3afa9f17c91acd2dc40cd8abaa9d16478b
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-18 15:26:05 +0900

    Intro UI 구현

---
+++ app/src/main/java/com/nunu/android_dating_app/auth/IntroActivity.kt
@@ -0,0 +1,14 @@
+package com.nunu.android_dating_app.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nunu.android_dating_app.R
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
