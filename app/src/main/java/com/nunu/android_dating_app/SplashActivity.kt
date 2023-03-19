599cae04f61b98803a1777cb4714c64c33f6a7f4
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-19 13:57:41 +0900

    UID가 null이면 로그인 화면 null이 아니면 Main 화면을 띄움

--- app/src/main/java/com/nunu/android_dating_app/SplashActivity.kt
+++ app/src/main/java/com/nunu/android_dating_app/SplashActivity.kt
@@ -4,21 +4,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.nunu.android_dating_app.auth.IntroActivity
+import com.nunu.android_dating_app.utils.FirebaseAuthUtils
 
 class SplashActivity : AppCompatActivity() {
+
+    private val TAG = "SplashActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Splash 화면 띄우기
-        Handler().postDelayed({
-            val intent = Intent(this, IntroActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
-            finish()
-        },3000)
+        // FirebaseAuthUtils 패키지에서 사용자 id를 가져오는 함수를 변수에 저장
+        val uid = FirebaseAuthUtils.getUid()
+
+        // uid가 null이면 IntroActivity로 전환
+        if(uid == "null"){
+
+            Log.d(TAG, "null")
+            Handler().postDelayed({
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()
+            },3000)
+
+        }
+        // uid가 null이 아니면 MainActivity로 전환
+        else{
+
+            Log.d(TAG, "not null")
+            Handler().postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()
+            },3000)
+
+        }
+
     }
 }
