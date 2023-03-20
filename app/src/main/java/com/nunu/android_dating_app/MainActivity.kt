1940cd5846548c513b42ad808454cfe7b4fa770e
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-20 17:15:15 +0900

    나와 다른 성별 매칭 기능 구현

--- app/src/main/java/com/nunu/android_dating_app/MainActivity.kt
+++ app/src/main/java/com/nunu/android_dating_app/MainActivity.kt
@@ -8,16 +8,19 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.nunu.android_dating_app.auth.IntroActivity
 import com.nunu.android_dating_app.auth.UserDataModel
 import com.nunu.android_dating_app.setting.MyPageActivity
 import com.nunu.android_dating_app.setting.SettingActivity
 import com.nunu.android_dating_app.slider.CardStackAdapter
+import com.nunu.android_dating_app.utils.FirebaseAuthUtils
 import com.nunu.android_dating_app.utils.FirebaseRef
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
@@ -36,6 +39,12 @@ class MainActivity : AppCompatActivity() {
     private val usersDataList = mutableListOf<UserDataModel>()
 
     private var userCount = 0
+
+    // 현재 유저의 성별 불러오기
+    private lateinit var currentUserGender : String
+
+    // UID 불러오기
+    private val uid = FirebaseAuthUtils.getUid()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -77,7 +86,7 @@ class MainActivity : AppCompatActivity() {
                 // 카드를 다 넘기면
                 if (userCount == usersDataList.count()){
                     // 다시 유저 정보를 받아옴
-                    getUserDataList()
+                    getUserDataList(currentUserGender)
                     Toast.makeText(this@MainActivity, "User Refresh",Toast.LENGTH_LONG).show()
                 }
 
@@ -105,13 +114,38 @@ class MainActivity : AppCompatActivity() {
         cardStackView.layoutManager = manager
         cardStackView.adapter = cardStackAdpater
 
-        // 회원정보 불러오는 함수 호출
-        getUserDataList()
+        // 나의 성별을 불러오는 함수 호출
+        getMyuserData()
+
+    }
+
+    // 나의 성별을 불러오는 함수
+    private fun getMyuserData(){
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                val data = dataSnapshot.getValue(UserDataModel::class.java)
+
+                currentUserGender = data?.gender.toString()
+
+                // 회원정보 불러오는 함수 호출
+                getUserDataList(currentUserGender)
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+
+        // userInfoRef에 있는 데이터 불러오기
+        FirebaseRef.userInfoRef.child(uid).addValueEventListener(postListener)
 
     }
 
     // 회원정보 받아오는 함수
-    private fun getUserDataList(){
+    private fun getUserDataList(currentUserGender : String){
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
@@ -121,7 +155,17 @@ class MainActivity : AppCompatActivity() {
 
                     // user 정보를 리스트에 추가
                     val user = dataModel.getValue(UserDataModel::class.java)
-                    usersDataList.add(user!!)
+
+                    if (user!!.gender.toString().equals(currentUserGender)){
+
+                    }
+                    // 나의 성별과 다르면 다른 성별의 유저를 리스트에 저장
+                    else{
+
+                        usersDataList.add(user!!)
+
+                    }
+
                 }
 
                 // 정보 최신화
