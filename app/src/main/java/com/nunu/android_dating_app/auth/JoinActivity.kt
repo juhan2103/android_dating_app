413639584f4e64057d33a3c0e29aadb081269701
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-20 13:31:19 +0900

    유저 정보를 데이터베이스에 저장

--- app/src/main/java/com/nunu/android_dating_app/auth/JoinActivity.kt
+++ app/src/main/java/com/nunu/android_dating_app/auth/JoinActivity.kt
@@ -8,15 +8,24 @@ import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.nunu.android_dating_app.MainActivity
 import com.nunu.android_dating_app.R
+import com.nunu.android_dating_app.utils.FirebaseRef
 
 class JoinActivity : AppCompatActivity() {
 
     private var TAG = "JoinActivity"
 
     private lateinit var auth : FirebaseAuth
+
+    // 닉네임 성별 지역 나이 UID
+    private var nickname = ""
+    private var gender = ""
+    private var city = ""
+    private var age = ""
+    private var uid = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -34,13 +43,33 @@ class JoinActivity : AppCompatActivity() {
             val email = findViewById<TextInputEditText>(R.id.emailArea)
             val pwd = findViewById<TextInputEditText>(R.id.pwdArea)
 
+            nickname = findViewById<TextInputEditText>(R.id.nicknameArea).text.toString()
+            gender = findViewById<TextInputEditText>(R.id.genderArea).text.toString()
+            city = findViewById<TextInputEditText>(R.id.cityArea).text.toString()
+            age = findViewById<TextInputEditText>(R.id.ageArea).text.toString()
+
+
             // 회원가입으로 유저 생성
             auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
                 .addOnCompleteListener(this) { task ->
                     // 회원가입이 성공했을 때
                     if (task.isSuccessful) {
-                        Log.d(TAG, "createUser")
 
+                        // uid 받아오기
+                        val user = auth.currentUser
+                        uid = user?.uid.toString()
+
+                        val userModel = UserDataModel(
+                            uid,
+                            nickname,
+                            age,
+                            gender,
+                            city
+                        )
+
+                        FirebaseRef.userInfoRef.child(uid).setValue(userModel)
+
+                        // MainActivity로 화면 전환
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
 
