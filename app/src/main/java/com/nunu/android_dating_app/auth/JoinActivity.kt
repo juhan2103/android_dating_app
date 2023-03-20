bbd106edd95d0731342b5d773ce4b5c7a6789271
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-20 15:02:53 +0900

    이메일 공란 체크

--- app/src/main/java/com/nunu/android_dating_app/auth/JoinActivity.kt
+++ app/src/main/java/com/nunu/android_dating_app/auth/JoinActivity.kt
@@ -1,11 +1,14 @@
 package com.nunu.android_dating_app.auth
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.textfield.TextInputEditText
@@ -13,9 +16,11 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.nunu.android_dating_app.MainActivity
 import com.nunu.android_dating_app.R
 import com.nunu.android_dating_app.utils.FirebaseRef
+import java.io.ByteArrayOutputStream
 
 class JoinActivity : AppCompatActivity() {
 
@@ -30,6 +35,9 @@ class JoinActivity : AppCompatActivity() {
     private var age = ""
     private var uid = ""
 
+    // 회원가입 화면 프로필 이미지
+    lateinit var profileImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -39,8 +47,7 @@ class JoinActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
-        // 회원가입 화면 프로필 이미지
-        val profileImage = findViewById<ImageView>(R.id.imageArea)
+        profileImage = findViewById(R.id.imageArea)
 
         // 갤러리 불러오는 함수
         val getAction = registerForActivityResult(
@@ -62,6 +69,13 @@ class JoinActivity : AppCompatActivity() {
 
             val email = findViewById<TextInputEditText>(R.id.emailArea)
             val pwd = findViewById<TextInputEditText>(R.id.pwdArea)
+
+            val emailCheck = email.text.toString()
+
+            // email 입력란이 비어있을때
+            if(emailCheck.isEmpty()){
+                Toast.makeText(this, "이메일이 입력되지 않았습니다", Toast.LENGTH_LONG).show()
+            }
 
             nickname = findViewById<TextInputEditText>(R.id.nicknameArea).text.toString()
             gender = findViewById<TextInputEditText>(R.id.genderArea).text.toString()
@@ -87,7 +101,11 @@ class JoinActivity : AppCompatActivity() {
                             city
                         )
 
+                        // 유저 정보를 데이터베이스에 저장
                         FirebaseRef.userInfoRef.child(uid).setValue(userModel)
+
+                        // 프로필 이미지를 해당 유저의 storage에 저장
+                        uploadImage(uid)
 
                         // MainActivity로 화면 전환
                         val intent = Intent(this, MainActivity::class.java)
@@ -101,5 +119,27 @@ class JoinActivity : AppCompatActivity() {
                 }
         }
 
+    }
+
+    // storage에 이미지를 저장 함수
+    private fun uploadImage(uid : String){
+
+        val storage = Firebase.storage
+        val storageRef = storage.reference.child(uid + ".png")
+
+        profileImage.isDrawingCacheEnabled = true
+        profileImage.buildDrawingCache()
+        val bitmap = (profileImage.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = storageRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
     }
 }
