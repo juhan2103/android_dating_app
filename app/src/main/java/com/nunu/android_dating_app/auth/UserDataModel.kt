56fa3c0f9f107ed7c2c239b216156e9b54226a4a
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-20 13:30:50 +0900

    유저 정보 데이터 모델 추가

---
+++ app/src/main/java/com/nunu/android_dating_app/auth/UserDataModel.kt
@@ -0,0 +1,12 @@
+package com.nunu.android_dating_app.auth
+
+// 유저 정보 데이터 모델
+data class UserDataModel (
+    val uid : String? = null,
+    val nickname : String? = null,
+    val age : String? = null,
+    val gender : String? = null,
+    val city : String? = null
+)
+
+
